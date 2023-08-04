b8ec7dd8a8eb110dfabba7ebf40c4ab964d69da3
matthewbrownlow <>
2023-08-04 15:51:24 +0000

    Add get_player_guess() function to retrieve the players guess as an integer

--- run.py
+++ run.py
@@ -11,11 +11,25 @@ logo = """
 |__/     \__/|__/  |__/ \_______/   \___/        |__/  \__/ \______/ |__/ |__/ |__/|_______/  \_______/|__/       |__/
 """
 
+
 def generate_secret_number():
     """
     Generate a number between 1 and 20.
     """
     return random.randint(1, 20)
+
+
+def get_player_guess():
+    """
+    Gets the player's guess as an integer.
+    """
+    while True:
+        try:
+            guess = int(input("Take a guess: "))
+            return guess
+        except: ValueError:
+            print("Invalid input. Please enter a valid number between 1 and 20.\n")
+
 
 def play_game():
     print(logo)
